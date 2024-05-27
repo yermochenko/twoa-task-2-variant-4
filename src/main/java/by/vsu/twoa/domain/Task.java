17db2af0f1748c473f52317022c981b0b43d6e1a
Sergey Yermochenko <yermochenko@gmail.com>
2024-05-27 14:03:19 +0300

    FR3 и FR4 завершены

--- src/main/java/by/vsu/twoa/domain/Task.java
+++ src/main/java/by/vsu/twoa/domain/Task.java
@@ -1,11 +1,21 @@
 package by.vsu.twoa.domain;
 
+import by.vsu.twoa.geometry.Circle;
+import by.vsu.twoa.geometry.Point;
+import by.vsu.twoa.geometry.Square;
+
 import java.util.Date;
 
 public class Task extends Entity {
 	private User owner;
 	private String name;
 	private Date created;
+	private Point vertex1;
+	private Point vertex2;
+	private Square squareVariant1;
+	private Circle inscribedCircleVariant1;
+	private Square squareVariant2;
+	private Circle inscribedCircleVariant2;
 
 	public User getOwner() {
 		return owner;
@@ -29,5 +39,53 @@ public class Task extends Entity {
 
 	public void setCreated(Date created) {
 		this.created = created;
+	}
+
+	public Point getVertex1() {
+		return vertex1;
+	}
+
+	public void setVertex1(Point vertex1) {
+		this.vertex1 = vertex1;
+	}
+
+	public Point getVertex2() {
+		return vertex2;
+	}
+
+	public void setVertex2(Point vertex2) {
+		this.vertex2 = vertex2;
+	}
+
+	public Square getSquareVariant1() {
+		return squareVariant1;
+	}
+
+	public void setSquareVariant1(Square squareVariant1) {
+		this.squareVariant1 = squareVariant1;
+	}
+
+	public Circle getInscribedCircleVariant1() {
+		return inscribedCircleVariant1;
+	}
+
+	public void setInscribedCircleVariant1(Circle inscribedCircleVariant1) {
+		this.inscribedCircleVariant1 = inscribedCircleVariant1;
+	}
+
+	public Square getSquareVariant2() {
+		return squareVariant2;
+	}
+
+	public void setSquareVariant2(Square squareVariant2) {
+		this.squareVariant2 = squareVariant2;
+	}
+
+	public Circle getInscribedCircleVariant2() {
+		return inscribedCircleVariant2;
+	}
+
+	public void setInscribedCircleVariant2(Circle inscribedCircleVariant2) {
+		this.inscribedCircleVariant2 = inscribedCircleVariant2;
 	}
 }
--- src/main/java/by/vsu/twoa/service/TaskService.java
+++ src/main/java/by/vsu/twoa/service/TaskService.java
@@ -5,9 +5,12 @@ import by.vsu.twoa.dao.TaskDao;
 import by.vsu.twoa.dao.UserDao;
 import by.vsu.twoa.domain.Task;
 import by.vsu.twoa.domain.User;
+import by.vsu.twoa.geometry.*;
 import by.vsu.twoa.service.exception.ServiceException;
+import by.vsu.twoa.service.exception.TaskNotExistsException;
 import by.vsu.twoa.service.exception.UserNotExistsException;
 
+import java.util.Date;
 import java.util.List;
 
 public class TaskService {
@@ -28,6 +31,43 @@ public class TaskService {
 			List<Task> tasks = taskDao.readByOwner(id);
 			tasks.forEach(task -> task.setOwner(owner));
 			return tasks;
+		} catch(DaoException e) {
+			throw new ServiceException(e);
+		}
+	}
+
+	public Task findById(Integer id) throws ServiceException {
+		try {
+			Task task = taskDao.read(id).orElseThrow(() -> new TaskNotExistsException(id));
+			task.setOwner(userDao.read(task.getOwner().getId()).orElseThrow(() -> new UserNotExistsException(id)));
+			Point a = task.getVertex1();
+			Point b = task.getVertex2();
+			Vector v = new Vector(a, b);
+			double radius = v.length();
+			v = v.rotate(90);
+			Point c1 = v.put(b);
+			Point d1 = v.put(a);
+			v = v.multiply(-1);
+			Point c2 = v.put(b);
+			Point d2 = v.put(a);
+			task.setSquareVariant1(new Square(a, b, c1, d1));
+			task.setInscribedCircleVariant1(new Circle(new Segment(a, c1).middle(), radius));
+			task.setSquareVariant2(new Square(a, b, c2, d2));
+			task.setInscribedCircleVariant2(new Circle(new Segment(a, c2).middle(), radius));
+			return task;
+		} catch(DaoException e) {
+			throw new ServiceException(e);
+		}
+	}
+
+	public Integer save(Task task) throws ServiceException {
+		try {
+			if(task.getId() == null) {
+				task.setCreated(new Date(0));
+				return taskDao.create(task);
+			} else {
+				throw new RuntimeException("Update operation not implemented yet");
+			}
 		} catch(DaoException e) {
 			throw new ServiceException(e);
 		}
